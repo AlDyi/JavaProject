d8284a12ea35eb460e18b0ea570fae1c0b8a3680
AlDyi <48719055+AlDyi@users.noreply.github.com>
2021-07-15 23:59:41 +0300

    Add files via upload

--- LibraryProject/src/main/java/library/LibraryController.java
+++ LibraryProject/src/main/java/library/LibraryController.java
@@ -3,6 +3,7 @@ package library;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/library")
@@ -15,9 +16,13 @@ public class LibraryController {
     }
 
     @GetMapping
-    public String test1() throws IOException {
-        Books newBook = new Books();
-        return newBook.findBookAuthorOrName("Булгаков");
+    public String test1() throws IOException, ParseException {
+        LibraryCards cards = new LibraryCards();
+        Person human = new Person(1, "Дьякова Александра Андреевна");
+        Book book = new Book(116, "Мертвые души", "Н.В.Гоголь", 2);
+        //return cards.takeBook(human, book);
+        //return cards.returnBook(human, book);
+        return cards.renewBook(human, book, 2);
         //return newBook.deleteBook(111);
     }
 }
